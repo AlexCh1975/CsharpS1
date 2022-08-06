4321713503438aa715c43119e0ad9ed9e2cdb8c1
AlexCh <stovertikal@mail.ru>
2022-08-06 10:34:49 +0700

    cheng task 005

--- task005/Program.cs
+++ task005/Program.cs
@@ -8,15 +8,25 @@
 
 Console.Clear();
 
-void TransformNumber(string num)
+// void TransformNumber(string num)
+// {
+//     char[] numbers = num.ToCharArray();
+//     for (int i = 0; i <= numbers.Length - 1; i++)
+//     {
+//         if (i == numbers.Length -1) Console.WriteLine($"Последняя цифра числа {num}: {numbers[i]}");
+//     } 
+// }
+
+// Console.Write("Введите число: ");
+// string num = Console.ReadLine();
+// TransformNumber(num);
+
+void TransformNumber(int number)
 {
-    char[] numbers = num.ToCharArray();
-    for (int i = 0; i <= numbers.Length - 1; i++)
-    {
-        if (i == numbers.Length -1) Console.WriteLine($"Последняя цифра числа {num}: {numbers[i]}");
-    } 
+    int num = number % 10;
+    Console.WriteLine($"Последняя цифра числа {number}: {num}");
 }
 
 Console.Write("Введите число: ");
-string num = Console.ReadLine();
-TransformNumber(num);
+int number = Convert.ToInt32(Console.ReadLine());
+TransformNumber(number);
